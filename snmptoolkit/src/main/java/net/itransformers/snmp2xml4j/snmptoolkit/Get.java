0ea50ddca51553ea088d167ca3feec1d445b028d
niau <n.milovanov@gmail.com>
2016-03-14 13:43:48 +0200

    Initial deployment on issue #5

--- snmptoolkit/src/main/java/net/itransformers/snmp2xml4j/snmptoolkit/Get.java
+++ snmptoolkit/src/main/java/net/itransformers/snmp2xml4j/snmptoolkit/Get.java
@@ -32,10 +32,8 @@ import org.snmp4j.mp.CounterSupport;
 import org.snmp4j.mp.DefaultCounterListener;
 import org.snmp4j.mp.MPv3;
 import org.snmp4j.mp.SnmpConstants;
-import org.snmp4j.smi.OID;
-import org.snmp4j.smi.OctetString;
-import org.snmp4j.smi.UdpAddress;
-import org.snmp4j.smi.VariableBinding;
+import org.snmp4j.security.*;
+import org.snmp4j.smi.*;
 import org.snmp4j.transport.AbstractTransportMapping;
 import org.snmp4j.transport.DefaultUdpTransportMapping;
 
@@ -50,33 +48,71 @@ import java.util.Vector;
  */
 public class Get {
     static Logger logger = Logger.getLogger(Walk.class);
+    private  UsmUser user = null;
 
     String oid;
-    String community;
-    private UdpAddress localAddress;
+    USM usm;
+    private TransportIpAddress localAddress;
     private TransportMappingAbstractFactory transportFactory;
     private MessageDispatcherAbstractFactory messageDispatcherFactory;
-    CommunityTarget target = new CommunityTarget();
+    TransportMapping transport;
+    String securityName;
+    String engineId;
+
+    Target target;
+
     /**
-     * <p>Constructor for Get.</p>
+     * <p>Constructor for v1 and v2 SNMP Get.</p>
      *
      * @param oid a {@link java.lang.String} object.
      * @param target a {@link org.snmp4j.CommunityTarget} object.
      * @param transportFactory a {@link net.itransformers.snmp2xml4j.snmptoolkit.transport.TransportMappingAbstractFactory} object.
      * @param messageDispatcherAbstractFactory a {@link net.itransformers.snmp2xml4j.snmptoolkit.messagedispacher.MessageDispatcherAbstractFactory} object.
      */
-    public Get(String oid, CommunityTarget target,TransportMappingAbstractFactory transportFactory, MessageDispatcherAbstractFactory messageDispatcherAbstractFactory){
+    public Get(String oid, CommunityTarget target,TransportMappingAbstractFactory transportFactory, MessageDispatcherAbstractFactory messageDispatcherAbstractFactory)  {
         this.oid = oid;
-
         this.target = target;
+
         this.transportFactory = transportFactory;
         this.messageDispatcherFactory = messageDispatcherAbstractFactory;
         this.transportFactory = transportFactory;
-        localAddress = new UdpAddress("0.0.0.0/0");
+        if (target.getAddress() instanceof UdpAddress) {
+            localAddress = new UdpAddress("0.0.0.0/0");
+        }else {
+            localAddress = new TcpAddress("0.0.0.0/0");
+        }
+        try {
+            transport = transportFactory.createTransportMapping(localAddress);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
 
     }
+
+    public Get(String oid, UserTarget target,TransportMappingAbstractFactory transportFactory, MessageDispatcherAbstractFactory messageDispatcherAbstractFactory)  {
+        this.oid = oid;
+        this.target = target;
+
+        this.transportFactory = transportFactory;
+        this.messageDispatcherFactory = messageDispatcherAbstractFactory;
+        this.transportFactory = transportFactory;
+        if (target.getAddress() instanceof UdpAddress) {
+            localAddress = new UdpAddress("0.0.0.0/0");
+        }else {
+            localAddress = new TcpAddress("0.0.0.0/0");
+        }
+        try {
+            transport = transportFactory.createTransportMapping(localAddress);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
     /**
-     * <p>Constructor for Get.</p>
+     * <p>Constructor for v1 and v2 SNMP Get</p>
      *
      * @param oid a {@link java.lang.String} object.
      * @param address a {@link java.lang.String} object.
@@ -99,19 +135,114 @@ public class Get {
     ) throws IOException {
 
         this.oid = oid;
-        target = new CommunityTarget();
-        target.setAddress(new UdpAddress(address));
-        target.setRetries(retries);
-        target.setTimeout(timeout);
-        target.setVersion(version);
-        target.setMaxSizeRequestPDU(65535);
-        target.setCommunity(new OctetString(community));
+        CommunityTarget targett = new CommunityTarget();
+        targett.setAddress(new UdpAddress(address));
+        targett.setRetries(retries);
+        targett.setTimeout(timeout);
+        targett.setVersion(version);
+        targett.setMaxSizeRequestPDU(65535);
+        targett.setCommunity(new OctetString(community));
+        this.target = targett;
 
-        this.community = community;
         this.transportFactory = transportFactory;
         this.messageDispatcherFactory = messageDispatcherAbstractFactory;
-        localAddress = new UdpAddress("0.0.0.0/0");
+        if (this.target.getAddress() instanceof UdpAddress) {
+            this.localAddress = new UdpAddress("0.0.0.0/0");
+        }else {
+            this.localAddress = new TcpAddress("0.0.0.0/0");
+        }
+        try {
+
+        this.transport = transportFactory.createTransportMapping(localAddress);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
+    public Get(String oid, TransportMappingAbstractFactory transportFactory, MessageDispatcherAbstractFactory messageDispatcherAbstractFactory){
+        this.oid = oid;
+        this.target = new UserTarget();
+        this.transportFactory = transportFactory;
+        this.messageDispatcherFactory = messageDispatcherAbstractFactory;
+        this.transportFactory = transportFactory;
+        if (target.getAddress() instanceof UdpAddress) {
+            localAddress = new UdpAddress("0.0.0.0/0");
+        }else {
+            localAddress = new TcpAddress("0.0.0.0/0");
+        }
+        try {
+
+            this.transport = transportFactory.createTransportMapping(localAddress);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+    public Get(String oid,
+               String address,
+               String securityEngineId,
+               String securityName,
+               String authenticationPassShare,
+               String authenticationProtocol,
+               String privacyProtocol,
+               String privacyProtocolPassShare,
+               int version,
+               int retries,
+               long timeout,
+               TransportMappingAbstractFactory transportFactory,
+               MessageDispatcherAbstractFactory messageDispatcherAbstractFactory
+    ) throws IOException {
+
+        this.oid = oid;
+        this.securityName = securityName;
+        UserTarget targett = new UserTarget();
+        UdpAddress udpAddress = new UdpAddress(address+"/161");
+        targett.setAuthoritativeEngineID(hexStringToByteArray(securityEngineId));
+        targett.setAddress(udpAddress);
+        targett.setRetries(retries);
+        targett.setTimeout(timeout);
+        targett.setVersion(version);
+        targett.setMaxSizeRequestPDU(65535);
+        targett.setSecurityLevel(SecurityLevel.AUTH_NOPRIV);
+        targett.setSecurityName(new OctetString(securityName));
+        this.target = targett;
+
+
+
+        OID authenticationProtocolOID = null;
+
+        if ("MD5".equals(authenticationProtocol)){
+            authenticationProtocolOID=AuthMD5.ID;
+        }else if ("SHA".equals(authenticationProtocol)){
+            authenticationProtocolOID= AuthSHA.ID;
+        }
+        OID privacyProtocolOID = null;
+
+        if ("DES".equals(privacyProtocol)){
+            privacyProtocolOID= PrivDES.ID;
+        }else if ("3DES".equals(privacyProtocol)) {
+            privacyProtocolOID= Priv3DES.ID;
+        }
+
+        this.usm = new USM(SecurityProtocols.getInstance(), new OctetString(MPv3.createLocalEngineID()), 0);
+
+        user =  new UsmUser(new OctetString(securityName), authenticationProtocolOID, new OctetString(authenticationPassShare), null, null);
+
+        usm.addUser(new OctetString(securityName), user);
+
+        this.transportFactory = transportFactory;
+        this.messageDispatcherFactory = messageDispatcherAbstractFactory;
+
+        if (target.getAddress() instanceof UdpAddress) {
+            localAddress = new UdpAddress("0.0.0.0/0");
+        }else {
+            localAddress = new TcpAddress("0.0.0.0/0");
+        }
+        this.transport = transportFactory.createTransportMapping(localAddress);
+
+    }
+
+
+
 
     /**
      * <p>getSNMPValue.</p>
@@ -121,22 +252,36 @@ public class Get {
      */
     public String getSNMPValue() throws IOException {
 
+
         String result = "";
         CounterSupport.getInstance().addCounterListener(new DefaultCounterListener());
         VariableBinding vb = new VariableBinding(new OID(this.oid));
         Vector vbs = new Vector();
         vbs.add(vb);
-        TransportMapping transport = transportFactory.createTransportMapping(localAddress);
-        MessageDispatcher dispatcher = messageDispatcherFactory.createMessageDispatcherMapping();
-        Snmp snmp = new Snmp(dispatcher, transport);
-        ((MPv3) snmp.getMessageProcessingModel(MPv3.ID)).setLocalEngineID(new OctetString(MPv3.createLocalEngineID()).getValue());
 
+        Snmp snmp = createSession();
+        if (target.getVersion() == SnmpConstants.version3){
+
+            //UserTarget targett = (UserTarget)target;
+
+            SecurityModels.getInstance().addSecurityModel(usm);
+
+
+            snmp.getUSM().addUser(new OctetString(securityName), user);
+            //snmp.discoverAuthoritativeEngineID(targett.getAuthoritativeEngineID(),targett.getTimeout());
+
+        }
 
         snmp.listen();
 
         try {
 
-            PDU request = new PDU();
+            PDU request;
+            if (target.getVersion() == SnmpConstants.version3){
+              request = new ScopedPDU();
+            } else {
+               request =new PDU();
+            }
             request.setType(PDU.GET);
 
             for (int i = 0; i < vbs.size(); i++) {
@@ -144,9 +289,14 @@ public class Get {
             }
 
             long startTime = System.currentTimeMillis();
-            ResponseEvent responseEvent = snmp.send(request, target, transport);
+
 
             PDU response = null;
+
+            ResponseEvent responseEvent = snmp.send(request, target);
+
+
+
             if (responseEvent != null) {
                 response = responseEvent.getResponse();
                 logger.debug("Received response after " + (System.currentTimeMillis() - startTime) + " millis");
@@ -180,17 +330,13 @@ public class Get {
     public String getSNMPGetNextValue() throws IOException {
 
     String result = "";
-    CounterSupport.getInstance().addCounterListener(new DefaultCounterListener());
+    //CounterSupport.getInstance().addCounterListener(new DefaultCounterListener());
+
     VariableBinding vb = new VariableBinding(new OID(this.oid));
-    //        VariableBinding vb = new VariableBinding(new OID("1.3.6.1.2.1.31.1.1.1"));
     Vector vbs = new Vector();
     vbs.add(vb);
-    TransportMapping transport = transportFactory.createTransportMapping(localAddress);
-    MessageDispatcher dispatcher = messageDispatcherFactory.createMessageDispatcherMapping();
-    //        AbstractTransportMapping transport = new DefaultUdpTransportMapping(localAddress);
-    Snmp snmp = new Snmp(dispatcher, transport);
-    ((MPv3) snmp.getMessageProcessingModel(MPv3.ID)).setLocalEngineID(new OctetString(MPv3.createLocalEngineID()).getValue());
 
+    Snmp snmp = createSession();
     snmp.listen();
 
     try {
@@ -228,7 +374,21 @@ public class Get {
         snmp.close();
     }
 }
+    private Snmp createSession() throws IOException {
 
+
+        MessageDispatcher dispatcher = messageDispatcherFactory.createMessageDispatcherMapping();
+        //        AbstractTransportMapping transport = new DefaultUdpTransportMapping(localAddress);
+        Snmp snmp = new Snmp(dispatcher, transport);
+
+
+
+        ((MPv3) snmp.getMessageProcessingModel(MPv3.ID)).setLocalEngineID(new OctetString(MPv3.createLocalEngineID()).getValue());
+
+        return snmp;
+
+
+    }
 
     /**
      * <p>main.</p>
@@ -255,7 +415,7 @@ public class Get {
         target.setAddress(new UdpAddress("192.168.107.33/11161"));
 
         target.setRetries(1);
-        target.setTimeout(1000);
+        target.setTimeout(2000);
         target.setMaxSizeRequestPDU(65535);
         snmp.listen();
 
@@ -270,17 +430,12 @@ public class Get {
         ResponseEvent responseEvent = snmp.send(request, target);
 
         PDU response = null;
+
         if (responseEvent != null) {
             response = responseEvent.getResponse();
             System.out.println("Received response after " + (System.currentTimeMillis() - startTime) + " millis");
         }
 
-        System.out.println("Response received with requestID=" +
-                response.getRequestID() +
-                ", errorIndex=" +
-                response.getErrorIndex() + ", " +
-                "errorStatus=" + response.getErrorStatusText() +
-                "(" + response.getErrorStatus() + ")");
 
         for (int i = 0; i < response.size(); i++) {
             VariableBinding vb1 = response.get(i);
@@ -289,6 +444,15 @@ public class Get {
         snmp.close();
 
 
+    }
+    private static byte[] hexStringToByteArray(String s) {
+        int len = s.length();
+        byte[] data = new byte[len / 2];
+        for (int i = 0; i < len; i += 2) {
+            data[i / 2] = (byte) ((Character.digit(s.charAt(i), 16) << 4)
+                    + Character.digit(s.charAt(i+1), 16));
+        }
+        return data;
     }
 
 }
---
+++ snmptoolkit/src/main/java/net/itransformers/snmp2xml4j/snmptoolkit/SnmpManager.java
@@ -0,0 +1,236 @@
+/*
+ * SnmpManager.java
+ *
+ * This work is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published
+ * by the Free Software Foundation; either version 2 of the License,
+ * or (at your option) any later version.
+ *
+ * This work is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+ * USA
+ *
+ * Copyright (c) 2010-2016 iTransformers Labs. All rights reserved.
+ */
+
+package net.itransformers.snmp2xml4j.snmptoolkit;
+
+/**
+ * Created by niau on 3/14/16.
+ */
+import org.snmp4j.*;
+import org.snmp4j.event.ResponseEvent;
+import org.snmp4j.mp.MPv3;
+import org.snmp4j.mp.SnmpConstants;
+import org.snmp4j.security.*;
+import org.snmp4j.smi.*;
+import org.snmp4j.transport.DefaultUdpTransportMapping;
+
+import java.io.IOException;
+
+public class SnmpManager{
+    Snmp snmp    = null;
+    String address    = null;
+
+    int  SNMPversion   = 3;
+
+    String ver3Username  = "usr-md5-none";
+    String ver3AuthPasscode = "authkey1";
+
+    /**
+     * Constructor
+     *
+     * @param add
+     */
+
+    public SnmpManager(String add)
+    {
+        address = add;
+    }
+
+    /**
+     * Start the Snmp session. If you forget the listen() method you will not get any answers because the communication is asynchronous and the
+     * listen() method listens for answers.
+     *
+     * @throws IOException
+     */
+
+    public void start() throws IOException {
+        TransportMapping transport = new DefaultUdpTransportMapping();
+
+        if (SNMPversion == 3) {
+            USM usm = new USM(SecurityProtocols.getInstance(), new OctetString(MPv3.createLocalEngineID()), 0);
+            SecurityModels.getInstance().addSecurityModel(usm);
+        }
+        snmp = new Snmp(transport);
+
+        if (SNMPversion == 3)
+            snmp.getUSM().addUser(new OctetString(ver3Username),
+                    new UsmUser(new OctetString(ver3Username), AuthMD5.ID, new OctetString(ver3AuthPasscode), null, null));
+
+        // Do not forget this line!
+        transport.listen();
+    }
+
+    /**
+     * Method which takes a single OID and returns the response from the agent as a String.
+     *
+     * @param oid
+     * @return
+     * @throws IOException
+     */
+
+    public String getAsString(OID oid) throws IOException {
+        ResponseEvent event = get(new OID[] { oid });
+        return event.getResponse().get(0).getVariable().toString();
+    }
+
+    public String setIntFromString(int value, OID oid) throws IOException {
+
+        ResponseEvent event = set(new OID[] { oid }, value);
+
+        return event.getResponse().get(0).getVariable().toString();
+
+    }
+
+    public ResponseEvent set(OID oids[], int value) throws IOException {
+
+        PDU pdu;
+
+        if (SNMPversion == 3)
+            pdu = new ScopedPDU();
+        else
+            pdu = new PDU();
+
+        for (OID oid : oids) {
+            pdu.add(new VariableBinding(oid, new Integer32(value)));
+        }
+
+        pdu.setType(PDU.SET);
+        ResponseEvent event = null;
+
+        try {
+            if (SNMPversion == 3)
+                event = snmp.send(pdu, getSNMPv3Target(), null);
+            else
+                event = snmp.send(pdu, getTarget(), null);
+
+        } catch (IOException ioe) {
+            System.out.println("Error SNMP SET");
+        }
+
+        if (event != null) {
+            return event;
+        }
+        throw new RuntimeException("SET timed out");
+    }
+
+    /**
+     * This method is capable of handling multiple OIDs
+     *
+     * @param oids
+     * @return
+     * @throws IOException
+     */
+
+    public ResponseEvent get(OID oids[]) throws IOException {
+        PDU pdu;
+
+        if (SNMPversion == 3)
+            pdu = new ScopedPDU();
+        else
+            pdu = new PDU();
+
+        for (OID oid : oids) {
+            pdu.add(new VariableBinding(oid));
+        }
+
+        pdu.setType(PDU.GET);
+
+        ResponseEvent response;
+
+        if (SNMPversion == 3)
+            response = snmp.send(pdu, getSNMPv3Target());
+        else
+            response = snmp.send(pdu, getTarget());
+
+        if (response != null) {
+            PDU responsePDU = response.getResponse();
+            if (responsePDU != null) {
+                if (responsePDU.getErrorStatus() == PDU.noError) {
+                    return response;
+                }
+            }
+            throw new RuntimeException("reposne was null");
+        }
+        throw new RuntimeException("GET timed out");
+    }
+
+    /**
+     * This method returns a Target, which contains information about where the data should be fetched and how.
+     *
+     * @return
+     */
+
+    private Target getSNMPv3Target() {
+        Address targetAddress = GenericAddress.parse(address);
+        UserTarget target = new UserTarget();
+
+        target.setAddress(targetAddress);
+
+        target.setVersion(SnmpConstants.version3); // SnmpConstants.version3
+        target.setRetries(2);
+        target.setTimeout(2500);
+        target.setSecurityLevel(SecurityLevel.AUTH_NOPRIV); // SecurityLevel.AUTH_NOPRIV
+        target.setSecurityName(new OctetString(ver3Username));
+
+        return target;
+    }
+
+    private Target getTarget() {
+
+        Address targetAddress = GenericAddress.parse(address);
+        CommunityTarget target = new CommunityTarget();
+
+        target.setCommunity(new OctetString("public"));
+
+        target.setAddress(targetAddress);
+
+        target.setRetries(2);
+
+        target.setTimeout(1500);
+
+        target.setVersion(SnmpConstants.version2c);
+
+        return target;
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        SnmpManager snmpManager = new SnmpManager("195.218.195.228/161");
+
+        String oid1 = "1.3.6.1.2.1.1.1.0";
+        snmpManager.start();
+        OID oid = new OID(oid1);
+        OID oids[] = new OID[]{oid};
+        ResponseEvent responseEvent = snmpManager.get(oids);
+        PDU response = responseEvent.getResponse();
+
+        for (int i = 0; i < response.size(); i++) {
+            VariableBinding vb1 = response.get(i);
+            System.out.println(vb1.toString());
+        }
+
+
+        //System.out.println(responseEvent.getResponse().toString());
+
+
+    }
+
+}
